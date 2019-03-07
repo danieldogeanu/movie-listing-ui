9f4696de197bc66ee1d63a9f66e2b451fd7d586c
Daniel Dogeanu <contact@danieldogeanu.com>
2019-03-07 14:06:46 +0200

    Added SCSS Includes

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,14 @@ export default {
 </script>
 
 <style lang="scss">
+@import './styles/includes';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: color-scheme(global, text);
   margin-top: 60px;
 }
 </style>
