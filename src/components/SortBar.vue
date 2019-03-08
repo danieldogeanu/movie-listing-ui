bd50b9647b81d76eb8abc2a41339e738197f97b8
Daniel Dogeanu <contact@danieldogeanu.com>
2019-03-08 05:21:34 +0200

    Made SortBar Responsive

--- src/components/SortBar.vue
+++ src/components/SortBar.vue
@@ -31,23 +31,39 @@ export default {
 
 %sortbtn {
 	cursor: pointer;
-	margin: 0 rem(20);
+	margin: 0 rem(5);
 	border: 0 none;
 	background: transparent;
 	font-family: font(open-sans);
 	font-weight: weight(regular);
-	font-size: rem(16);
+	font-size: rem(12);
 	text-transform: uppercase;
 	color: color-scheme(sortbar, btn);
 	transition: color .2s ease;
+	@include breakpoint(min, rem(481)) {
+		margin: 0 rem(10);
+		font-size: rem(14);
+	}
+	@include breakpoint(min, rem(601)) {
+		margin: 0 rem(20);
+		font-size: rem(16);
+	}
 	&:hover {
 		color: color-scheme(sortbar, hover);
 	}
 }
 
 %sortbtn__icon {
-	width: rem(40);
-	height: rem(40);
+	width: rem(30);
+	height: rem(30);
+	@include breakpoint(min, rem(481)) {
+		width: rem(35);
+		height: rem(35);
+	}
+	@include breakpoint(min, rem(601)) {
+		width: rem(40);
+		height: rem(40);
+	}
 }
 
 .sortbar {
@@ -55,6 +71,16 @@ export default {
 	justify-content: space-between;
 	align-items: center;
 	margin-bottom: rem(20);
+
+	&__sort {
+		padding: 0 rem(15);
+		@include breakpoint(min, rem(481)) {
+			padding: 0 rem(10);
+		}
+		@include breakpoint(min, rem(601)) {
+			padding: 0;
+		}
+	}
 
 	&__dir {
 		padding: 0 rem(15);
