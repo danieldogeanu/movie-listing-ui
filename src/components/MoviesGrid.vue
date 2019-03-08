12c5d926d12c015c05f1126c47fd70a271704ea1
Daniel Dogeanu <contact@danieldogeanu.com>
2019-03-08 05:08:41 +0200

    Made MoviesGrid Responsive

--- src/components/MoviesGrid.vue
+++ src/components/MoviesGrid.vue
@@ -20,7 +20,19 @@ export default {
 
 .moviesgrid {
 	display: grid;
-	grid-template: auto / repeat(4, 1fr);
-	gap: rem(60) rem(30);
+	grid-template: auto / 1;
+	gap: rem(30) 0;
+	@include breakpoint(min, rem(421)) {
+		grid-template: auto / repeat(2, 1fr);
+		gap: rem(40) rem(20);
+	}
+	@include breakpoint(min, rem(641)) {
+		grid-template: auto / repeat(3, 1fr);
+		gap: rem(50) rem(25);
+	}
+	@include breakpoint(min, rem(861)) {
+		grid-template: auto / repeat(4, 1fr);
+		gap: rem(60) rem(30);
+	}
 }
 </style>
