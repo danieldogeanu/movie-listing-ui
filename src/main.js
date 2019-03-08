fdd473ee12e143f62796e623c7f14bb2336baedb
Daniel Dogeanu <contact@danieldogeanu.com>
2019-03-08 07:58:28 +0200

    Added Browser Upgrade

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import browserDetect from 'vue-browser-detect-plugin';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(browserDetect);
 
-new Vue({
+var vm = new Vue({
   render: h => h(App),
-}).$mount('#app')
+});
+
+// If current browser is not IE, remove browserupgrade message.
+(() => {
+  const html = document.querySelector('html').classList;
+  const browserupgrade = document.getElementById('browserupgrade');
+  const browser = vm.$browserDetect;
+
+  if (!browser.isIE) {
+    if (html.contains('ie')) {
+      html.remove('ie');
+      html.add(browser.meta.name.toLowerCase());
+      browserupgrade.remove();
+      vm.$mount('#app');
+    }
+  }
+})();
