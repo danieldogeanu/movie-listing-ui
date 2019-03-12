d19187a3d86bb13fdff3923800ab9647d6ee7330
Daniel Dogeanu <contact@danieldogeanu.com>
2019-03-12 13:21:06 +0200

    Added ErrorState Component

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,15 @@
         :theDir="sortDir"
         @keyChange="changeKey"
         @dirChange="changeDir"
-        @ratingChanged="changeRating"
-      />
+        @ratingChanged="changeRating" />
       <movies-grid 
+        v-if="isLoaded"
         :movies="filteredMovies" 
         :config="moviesConfig" 
         :details="moviesDetails" />
+      <error-state 
+        v-else-if="!isLoaded && error.state"
+        :error="error" />
       <the-footer />
     </div>
   </main>
@@ -23,12 +26,21 @@
 import Sprite from './components/Sprite.vue';
 import Header from './components/Header.vue';
 import SortBar from './components/SortBar.vue';
+import ErrorState from './components/ErrorState.vue';
 import MoviesGrid from './components/MoviesGrid.vue';
 import Footer from './components/Footer.vue';
 import tmdb from './tmdb.js';
 
 export default {
   name: 'app',
+  components: {
+    'sprite': Sprite,
+    'the-header': Header,
+    'sort-bar': SortBar,
+    'error-state': ErrorState,
+    'movies-grid': MoviesGrid,
+    'the-footer': Footer,
+  },
   data() {
     return {
       appTitle: 'Movies List',
@@ -38,6 +50,16 @@ export default {
       sortKey: 'popular',
       sortDir: 'desc',
       filterRating: 3,
+      error: {
+        state: false,
+        title: 'There seems to be a problem',
+        messages: [],
+      },
+      loaded: {
+        config: false,
+        movies: false,
+        details: false,
+      },
     }
   },
   computed: {
@@ -52,13 +74,11 @@ export default {
       return sorted.filter(movie => movie.vote_average > this.filterRating);
     },
 
-  },
-  components: {
-    'sprite': Sprite,
-    'the-header': Header,
-    'sort-bar': SortBar,
-    'movies-grid': MoviesGrid,
-    'the-footer': Footer,
+    // Check to see if all the data has loaded and that there are no errors.
+    isLoaded() {
+      return this.loaded.config && this.loaded.details && this.loaded.movies && !this.error.state;
+    },
+
   },
   methods: {
 
@@ -89,7 +109,9 @@ export default {
     // Check the response status and log the error if necessary.
     function checkResponse(res) {
       if (res.status !== 200) {
-        console.error('Looks like there was a problem with the request. Fetch response status code: ', res.status);
+        this.error.state = true;
+        this.error.messages.push('The TMDb server failed to respond. Try again later.');
+        this.error.messages.push('Status Code: ' + res.status);
         return;
       }
     }
@@ -100,8 +122,15 @@ export default {
         checkResponse(res);
         return res.json();
       })
-      .then(json => this.moviesConfig = json)
-      .catch(err => console.error('Fetch Error: ', err));
+      .then(json => {
+        this.moviesConfig = json;
+        this.loaded.config = true;
+      })
+      .catch(() => {
+        this.loaded.config = false;
+        this.error.state = true;
+        this.error.messages.push('Couldn\'t fetch the config.');
+      });
 
     // Second: Let's get the list with the Now Playing movies.
     fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=' + tmdb)
@@ -124,11 +153,20 @@ export default {
               checkResponse(res);
               return res.json();
             })
-            .then(json => this.moviesDetails.push(json))
-            .catch(err => console.error('Fetch Error: ', err));
+            .then(json => {
+              this.moviesDetails.push(json);
+              this.loaded.details = true;
+            })
+            .catch(() => this.loaded.details = false);
         });
+
+        this.loaded.movies = true;
       })
-      .catch(err => console.error('Fetch Error: ', err));
+      .catch(() => {
+        this.loaded.movies = false;
+        this.error.state = true;
+        this.error.messages.push('Couldn\'t fetch the movies.');
+      });
 
   }
 }
---
+++ src/components/ErrorState.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="errorstate">
+		<h2 class="errorstate__title">{{error.title}}</h2>
+		<div class="errorstate__content">
+			<p class="errorstate__msg" 
+				v-for="(msg, index) in error.messages"
+				:key="index">{{msg}}</p>
+			<p class="errorstate__msg">Please try again later.</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ErrorState',
+	props: {
+		error: Object,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+@import '../styles/includes';
+
+.errorstate {
+	padding: rem(60) 0;
+	text-align: center;
+
+	&__title {
+		font-size: rem(17);
+		font-weight: weight(bold);
+		text-transform: uppercase;
+		color: color-scheme(state, error);
+	}
+
+	&__content {
+		margin: rem(30) 0;
+	}
+
+	&__msg {
+		font-size: rem(16);
+		margin: rem(12) 0;
+	}
+}
+</style>
